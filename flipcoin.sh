789ae63dac464d1f02553463ce87fdd91ca8254c
9Shreya <shreys.agrawal@gmail.com>
2021-07-31 19:10:36 +0530

    head tail who wins

--- flipcoin.sh
+++ flipcoin.sh
@@ -9,3 +9,15 @@ else
 echo tails
 fi
 
+declare -a array
+
+array=(0 0)
+
+for (( index=0;index<10;index++ ))
+do
+random=$(( RANDOM%((1-0)+1)+0 ))
+array[$random]=$(( ${array[$random]}+1 ))
+done
+
+echo head occured ${array[1]}
+echo tails occured ${array[0]}
